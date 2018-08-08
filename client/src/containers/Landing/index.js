a4cad3b941aa91fc237ad1e2cf2c7d0ef59eb86a
Mariano Heller <marianofheller@gmail.com>
2018-08-08 14:39:54 -0300

    landing button

--- client/src/containers/Landing/index.js
+++ client/src/containers/Landing/index.js
@@ -1,19 +1,40 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import * as SC from './StyledComponents';
 
 
-const Landing = () => (
+const Landing = props => (
   <SC.LandingWrapper>
     <SC.Title>Uchjoby</SC.Title>
     <SC.Subtitle>Learn through flashcards</SC.Subtitle>
 
     <SC.ButtonsContainer>
-      <SC.ButtonLink to="/practice" text="Practice now!" />
+      <SC.ButtonLink
+        disabled={props.data.length === 0}
+        to="/practice"
+        text="Practice now!"
+      />
     </SC.ButtonsContainer>
 
   </SC.LandingWrapper>
 );
 
 
-export default Landing;
+Landing.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.shape({})),
+}
+
+Landing.defaultProps = {
+  data: [],
+}
+
+const mapStateToProps = ({ words }) => ({
+  data: words.arrData,
+});
+
+
+const mapDispatchToProps = () => ({});
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Landing);

9cf0c34fd39c899bf706f618a4845cf1c1bbb68a
Mariano Heller <marianofheller@gmail.com>
2018-08-08 12:24:15 -0300

    trim legacy stuff

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
-const mongoose = require('mongoose');
-const passport = require('passport');
 
 //.env file
 require('dotenv').config();
@@ -19,28 +16,9 @@ const api = require('./routes/api');
 //====================================================
 const app = express();
 
-// Mongoose stuff
-mongoose.Promise = global.Promise;
-/* mongoose.set('debug', true);
-mongoose.connect(process.env.MONGO_DB, (err) => { if(err) console.log("Connection error", err)} ); */
 
 // Logging
 app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"'));
-
-// Session & Passport
-app.use(require('express-session')({ 
-  cookie: { 
-    path: '/', 
-    httpOnly: false, 
-    secure: false, 
-    maxAge: null 
-  },
-  secret: 'random string', 
-  resave: true, 
-  saveUninitialized: true 
-}));
-app.use(passport.initialize());
-app.use(passport.session());
 
 // CORS
 app.use(cors({
@@ -60,12 +38,6 @@ app.use(express.static(path.join(__dirname, 'client', 'build')));
 //Routes
 app.use('/', index);
 app.use('/api', api);
-// app.use('/auth', auth);
-/* app.use('/words', (req, res, next) => {
-  if(!req.isAuthenticated() || !req.user._id ) return res.status(401).send("Unauthorized");
-  next();
-});
-app.use('/words', words); */
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
